82c2424fb565958eaaf580db1f60f2d671e5ff60
Dinu-Loredana <dinuloredana.m@gmail.com>
2022-08-09 13:56:58 +0100

    add height input and use DecimalPipe to format number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   name: string | undefined;
   date: string | undefined;
   amount: number | undefined;
+  height: number | undefined;
 
   onNameChange(value: string) {
     this.name = value;
@@ -18,5 +19,8 @@ export class AppComponent {
   }
   onAmountChange(value: string) {
     this.amount = parseFloat(value);
+  }
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
   }
 }
