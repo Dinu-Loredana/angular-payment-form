f353772c9221708e8e73bdedd5c303bd4832a69a
Dinu-Loredana <dinuloredana.m@gmail.com>
2022-08-09 11:36:12 +0100

    setup project and create name input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-payment-form';
+  name = '';
+  onNameChange(value: string) {
+    this.name = value;
+  }
 }
